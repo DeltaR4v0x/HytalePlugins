ff0fa073bba088fbc38b06d60a78663ff98b83c4
Oliver <oschi1303@gmail.com>
2026-01-22 13:54:54 +0100

    fancycore: Add UI for balance top

--- plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/commands/economy/BalanceTopCMD.java
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/commands/economy/BalanceTopCMD.java
@@ -2,18 +2,22 @@ package com.fancyinnovations.fancycore.commands.economy;
 
 import com.fancyinnovations.fancycore.api.economy.Currency;
 import com.fancyinnovations.fancycore.api.economy.CurrencyService;
-import com.fancyinnovations.fancycore.api.economy.LeaderboardEntry;
 import com.fancyinnovations.fancycore.api.player.FancyPlayer;
 import com.fancyinnovations.fancycore.api.player.FancyPlayerService;
-import com.fancyinnovations.fancycore.utils.NumberUtils;
+import com.fancyinnovations.fancycore.uis.economy.BalancetopPage;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
-import org.jetbrains.annotations.NotNull;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
-import java.util.List;
+import javax.annotation.Nonnull;
 
-public class BalanceTopCMD extends CommandBase {
+public class BalanceTopCMD extends AbstractPlayerCommand {
 
     public BalanceTopCMD() {
         super("balancetop", "View the top balances for the primary currency.");
@@ -22,7 +26,7 @@ public class BalanceTopCMD extends CommandBase {
     }
 
     @Override
-    protected void executeSync(@NotNull CommandContext ctx) {
+    protected void execute(@Nonnull CommandContext ctx, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
         if (!ctx.isPlayer()) {
             ctx.sendMessage(Message.raw("This command can only be executed by a player."));
             return;
@@ -40,17 +44,25 @@ public class BalanceTopCMD extends CommandBase {
             return;
         }
 
-        List<LeaderboardEntry> topBalances = CurrencyService.get().getTopBalances(currency, 10);
-
-        fp.sendMessage("Top 10 Balances in " + currency.name() + ":");
-        if (topBalances.isEmpty()) {
-            fp.sendMessage("No balances found.");
+        Player player = store.getComponent(ref, Player.getComponentType());
+        if (player == null) {
+            ctx.sendMessage(Message.raw("Player component not found."));
             return;
         }
 
-        for (int i = 0; i < topBalances.size(); i++) {
-            LeaderboardEntry entry = topBalances.get(i);
-            fp.sendMessage(entry.rank() + ". " + entry.username() + " (" + currency.symbol() + NumberUtils.formatNumber(entry.balance()) + ") ");
-        }
+        player.getPageManager().openCustomPage(ref, store, new BalancetopPage(playerRef, currency));
+
+//        List<LeaderboardEntry> topBalances = CurrencyService.get().getTopBalances(currency, 10);
+//
+//        fp.sendMessage("Top 10 Balances in " + currency.name() + ":");
+//        if (topBalances.isEmpty()) {
+//            fp.sendMessage("No balances found.");
+//            return;
+//        }
+//
+//        for (int i = 0; i < topBalances.size(); i++) {
+//            LeaderboardEntry entry = topBalances.get(i);
+//            fp.sendMessage(entry.rank() + ". " + entry.username() + " (" + currency.symbol() + NumberUtils.formatNumber(entry.balance()) + ") ");
+//        }
     }
 }
---
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/uis/economy/BalancetopPage.java
@@ -0,0 +1,57 @@
+package com.fancyinnovations.fancycore.uis.economy;
+
+import com.fancyinnovations.fancycore.api.economy.Currency;
+import com.fancyinnovations.fancycore.api.economy.CurrencyService;
+import com.fancyinnovations.fancycore.api.economy.LeaderboardEntry;
+import com.fancyinnovations.fancycore.api.player.FancyPlayer;
+import com.fancyinnovations.fancycore.api.player.FancyPlayerService;
+import com.fancyinnovations.fancycore.uis.EmptyUIData;
+import com.fancyinnovations.fancycore.utils.NumberUtils;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
+import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
+import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.jetbrains.annotations.NotNull;
+
+public class BalancetopPage extends InteractiveCustomUIPage<EmptyUIData> {
+
+    private final Currency currency;
+
+    public BalancetopPage(@NotNull PlayerRef playerRef, Currency currency) {
+        super(playerRef, CustomPageLifetime.CanDismiss, EmptyUIData.CODEC);
+        this.currency = currency;
+    }
+
+    @Override
+    public void build(@NotNull Ref<EntityStore> ref, @NotNull UICommandBuilder command, @NotNull UIEventBuilder event, @NotNull Store<EntityStore> store) {
+        UUIDComponent uuidComponent = store.getComponent(ref, UUIDComponent.getComponentType());
+        if (uuidComponent == null) {
+            return;
+        }
+
+        FancyPlayer fp = FancyPlayerService.get().getByUUID(uuidComponent.getUuid());
+        if (fp == null) {
+            return;
+        }
+
+        command.append("Pages/BalancetopPage.ui");
+
+        command.clear("#BalancetopCards");
+        command.appendInline("#Main #BalancetopList", "Group #BalancetopCards { LayoutMode: Left; }");
+
+        int i = 0;
+        for (LeaderboardEntry entry : CurrencyService.get().getTopBalances(currency, 10)) {
+            command.append("#BalancetopCards", "Pages/BalancetopEntry.ui");
+
+            command.set("#BalancetopCards[" + i + "] #Username.Text", entry.username() + " (#" + entry.rank() + ")");
+            command.set("#BalancetopCards[" + i + "] #Balance.Text", currency.symbol() + NumberUtils.formatNumber(entry.balance()));
+
+            i++;
+        }
+    }
+}
