241a71fed4074a94a205fffbfe3e5350792713c6
Oliver <oschi1303@gmail.com>
2026-01-22 13:24:16 +0100

    fancycore: Add UI for playerlist

--- plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/commands/player/PlayerListCMD.java
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/commands/player/PlayerListCMD.java
@@ -2,12 +2,20 @@ package com.fancyinnovations.fancycore.commands.player;
 
 import com.fancyinnovations.fancycore.api.player.FancyPlayer;
 import com.fancyinnovations.fancycore.api.player.FancyPlayerService;
+import com.fancyinnovations.fancycore.uis.player.PlayerListPage;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
-import org.jetbrains.annotations.NotNull;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
-public class PlayerListCMD extends CommandBase {
+import javax.annotation.Nonnull;
+
+public class PlayerListCMD extends AbstractPlayerCommand {
 
 
     public PlayerListCMD() {
@@ -17,11 +25,30 @@ public class PlayerListCMD extends CommandBase {
     }
 
     @Override
-    protected void executeSync(@NotNull CommandContext ctx) {
-        ctx.sendMessage(Message.raw("Online Players:"));
-
-        for (FancyPlayer onlinePlayer : FancyPlayerService.get().getOnlinePlayers()) {
-            ctx.sendMessage(Message.raw("- " + onlinePlayer.getData().getUsername() + " (UUID: " + onlinePlayer.getData().getUUID().toString() + ")"));
+    protected void execute(@Nonnull CommandContext ctx, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
+        if (!ctx.isPlayer()) {
+            ctx.sendMessage(Message.raw("This command can only be executed by a player."));
+            return;
         }
+
+        FancyPlayer fp = FancyPlayerService.get().getByUUID(ctx.sender().getUuid());
+        if (fp == null) {
+            ctx.sendMessage(Message.raw("FancyPlayer not found."));
+            return;
+        }
+
+        Player player = store.getComponent(ref, Player.getComponentType());
+        if (player == null) {
+            ctx.sendMessage(Message.raw("Player component not found."));
+            return;
+        }
+
+        player.getPageManager().openCustomPage(ref, store, new PlayerListPage(playerRef));
+
+//        ctx.sendMessage(Message.raw("Online Players:"));
+//
+//        for (FancyPlayer onlinePlayer : FancyPlayerService.get().getOnlinePlayers()) {
+//            ctx.sendMessage(Message.raw("- " + onlinePlayer.getData().getUsername() + " (UUID: " + onlinePlayer.getData().getUUID().toString() + ")"));
+//        }
     }
 }
---
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/uis/player/PlayerListPage.java
@@ -0,0 +1,68 @@
+package com.fancyinnovations.fancycore.uis.player;
+
+import com.fancyinnovations.fancycore.api.economy.Currency;
+import com.fancyinnovations.fancycore.api.economy.CurrencyService;
+import com.fancyinnovations.fancycore.api.permissions.Group;
+import com.fancyinnovations.fancycore.api.player.FancyPlayer;
+import com.fancyinnovations.fancycore.api.player.FancyPlayerService;
+import com.fancyinnovations.fancycore.uis.EmptyUIData;
+import com.fancyinnovations.fancycore.utils.NumberUtils;
+import com.fancyinnovations.fancycore.utils.TimeUtils;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
+import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
+import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PlayerListPage extends InteractiveCustomUIPage<EmptyUIData> {
+
+    public PlayerListPage(@NotNull PlayerRef playerRef) {
+        super(playerRef, CustomPageLifetime.CanDismiss, EmptyUIData.CODEC);
+    }
+
+    @Override
+    public void build(@NotNull Ref<EntityStore> ref, @NotNull UICommandBuilder command, @NotNull UIEventBuilder event, @NotNull Store<EntityStore> store) {
+        UUIDComponent uuidComponent = store.getComponent(ref, UUIDComponent.getComponentType());
+        if (uuidComponent == null) {
+            return;
+        }
+
+        FancyPlayer fp = FancyPlayerService.get().getByUUID(uuidComponent.getUuid());
+        if (fp == null) {
+            return;
+        }
+
+        command.append("Pages/PlayerListPage.ui");
+
+        command.clear("#PlayerListCards");
+        command.appendInline("#Main #PlayerListList", "Group #PlayerListCards { LayoutMode: Left; }");
+
+        int i = 0;
+        for (FancyPlayer onlinePlayer : FancyPlayerService.get().getOnlinePlayers()) {
+            command.append("#PlayerListCards", "Pages/PlayerListEntry.ui");
+
+            String primaryGroupName = "None";
+            List<Group> groupSortedByWeight = onlinePlayer.getData().getGroupSortedByWeight();
+            if (!groupSortedByWeight.isEmpty()) {
+                primaryGroupName = groupSortedByWeight.getFirst().getName();
+            }
+
+            Currency primaryCurrency = CurrencyService.get().getPrimaryCurrency();
+            double balance = onlinePlayer.getData().getBalance(primaryCurrency);
+
+            command.set("#PlayerListCards[" + i + "] #Username.Text", onlinePlayer.getData().getUsername());
+            command.set("#PlayerListCards[" + i + "] #Group.Text", primaryGroupName);
+            command.set("#PlayerListCards[" + i + "] #Balance.Text", primaryCurrency.symbol() + NumberUtils.formatNumber(balance));
+            command.set("#PlayerListCards[" + i + "] #Playtime.Text", TimeUtils.formatTime(onlinePlayer.getData().getPlayTime()));
+
+            i++;
+        }
+    }
+}
